a3f6dc241bbaf70bd1f22c6ce668f3044ed2f5b0
raoely <rtfdev3@mixr.net>
2017-02-01 16:28:02 +0300

    structure de base

--- src/OD/BasicsBundle/Controller/DefaultController.php
+++ src/OD/BasicsBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace OD\BasicsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('ODBasicsBundle:Default:index.html.twig');
+    }
+    
+    /**
+     * 
+     * @Route("/menu")
+     */
+    public function menuAction()
+    {
+        $navigation = array(
+            array('link' => '#', 'title' => 'Overview'),
+            array('link' => '#', 'title' => 'Document storage'),
+            array('link' => '#', 'title' => 'History'),
+            array('link' => '#', 'title' => 'About')
+        );
+        return $this->render('ODBasicsBundle:Default:menu-top.html.twig', array('navigation' => $navigation));
     }
 }
